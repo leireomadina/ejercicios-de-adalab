d52e625d651227f50fb5f611e9189ed94341c1f9
Leire Ordeñana <leireomadina@gmail.com>
2020-09-30 20:12:19 +0200

    Add second js exercise

---
+++ modulo-2/leccion-01-ejercicio-02-arreglando-errores/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let leireAdress = "Abbey Road";
+// const leireAdress = "Calle Errementariena, 4";
+// can't use const if we want to reassign the variable value
+leireAdress = "Fifth avenue";
+
+document.querySelector("h1").innerHTML = "Arreglando errores";
