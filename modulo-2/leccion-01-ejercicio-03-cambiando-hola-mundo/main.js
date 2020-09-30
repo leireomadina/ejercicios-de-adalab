57729f07d0b12b25ce10764f88aaf5abdbfa7579
Leire Ordeñana <leireomadina@gmail.com>
2020-09-30 20:26:12 +0200

    Add js exercise 3

---
+++ modulo-2/leccion-01-ejercicio-03-cambiando-hola-mundo/main.js
@@ -0,0 +1,6 @@
+"use strict";
+
+//Selects the element which has a class named "title" and assigns it to a variable
+const titleElement = document.querySelector(".title");
+//adds a new value to its html syntax
+titleElement.innerHTML = titleElement.innerHTML + " mundo";
