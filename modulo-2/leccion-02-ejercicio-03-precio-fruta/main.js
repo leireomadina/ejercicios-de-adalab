b8f7670dd63813c32ba1709d5698b6f98cb0d6ed
Leire Ordeñana <leireomadina@gmail.com>
2020-10-01 17:44:56 +0200

    Initial commit

---
+++ modulo-2/leccion-02-ejercicio-03-precio-fruta/main.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const kiwisQuantity = 2;
+const kiwisPrice = 5;
+const kiwisTotal = kiwisQuantity * kiwisPrice;
+console.log("Los kiwis cuestan " + kiwisTotal + " €.");
+
+const pearQuantity = 3;
+const pearPrice = 2;
+const pearTotal = pearQuantity * pearPrice;
+console.log("Las peras cuestan " + pearTotal + " €.");
+
+const grapesQuantity = 2;
+const grapesPrice = 5;
+const grapesTotal = grapesQuantity * grapesPrice;
+console.log("Las uvas cuestan " + grapesTotal + " €.");
+
+const totalPurchase = kiwisTotal + pearTotal + grapesTotal;
+console.log("La suma total de la compra son " + totalPurchase + " €.");
+
