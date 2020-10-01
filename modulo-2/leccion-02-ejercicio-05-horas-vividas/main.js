a9f313cb0bebf76178680583e8416abb3343881c
Leire Ordeñana <leireomadina@gmail.com>
2020-10-01 18:06:17 +0200

    Fix typo

--- modulo-2/leccion-02-ejercicio-05-horas-vividas/main.js
+++ modulo-2/leccion-02-ejercicio-05-horas-vividas/main.js
@@ -5,4 +5,4 @@ const dayHours = 24;
 const yearHours = 365;
 const totalHours = myAge * (dayHours * yearHours);
 
-console.log("He vidido en total " + totalHours + " horas.");
+console.log("He vivido en total " + totalHours + " horas.");
