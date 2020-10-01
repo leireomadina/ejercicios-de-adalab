e260969c335b1ac5a1bea549026df49c9173bb82
Leire Ordeñana <leireomadina@gmail.com>
2020-10-01 17:21:02 +0200

    Initial commit

---
+++ modulo-2/leccion-02-ejercicio-02-isra-mola/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const superWave = document.querySelector('.super-wave');
+
+superWave.classList.add('highlighted');
+
+console.log('superWave: ', superWave);
+console.log(superWave.innerHTML); // returns OLA!
