f8810162d0a548a7a3bf1d6cb8ed3a2f02d21bfa
Leire Ordeñana <leireomadina@gmail.com>
2020-10-01 18:03:30 +0200

    Initial commit

---
+++ modulo-2/leccion-02-ejercicio-05-horas-vividas/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let myAge = 32;
+const dayHours = 24;
+const yearHours = 365;
+const totalHours = myAge * (dayHours * yearHours);
+
+console.log("He vidido en total " + totalHours + " horas.");
