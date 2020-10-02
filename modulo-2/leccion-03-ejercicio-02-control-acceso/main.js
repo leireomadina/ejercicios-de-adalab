5a19b2d6a7dc032be209b18629e84840e12f6274
Leire Ordeñana <leireomadina@gmail.com>
2020-10-02 17:35:27 +0200

    Initial commit

---
+++ modulo-2/leccion-03-ejercicio-02-control-acceso/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const pairName = "Milda";
+
+if (pairName === "Milda") {
+  console.log(`Bienvenida, ${pairName}`);
+} else {
+  console.log("Lo siento pero el usuario que has introducido no está registrado");
+}
