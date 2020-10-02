573a4a50be21adc5424b258c7a667c53168972f9
Leire Ordeñana <leireomadina@gmail.com>
2020-10-02 17:19:23 +0200

    Initial commit

--- modulo-2/leccion-03-ejercicio-01-nadie-sin-avatar/main.js
+++ modulo-2/leccion-03-ejercicio-01-nadie-sin-avatar/main.js
@@ -4,10 +4,11 @@
 const DEFAULT_AVATAR = "http://placehold.it/300x300";
 // avatar que eligió el usuario al registrarse
 let userAvatar = "http://www.fillmurray.com/300/300";
+// let userAvatar = "";
 
 // gets the img element
 const avatarElement = document.querySelector(".user__avatar");
 // creates a src attribute
-avatarElement.setAttribute("src", userAvatar);
-// avatarElement.src = userAvatar;
+avatarElement.setAttribute("src", userAvatar || DEFAULT_AVATAR);
+// avatarElement.src = userAvatar || DEFAULT_AVATAR;
 console.log(avatarElement);
