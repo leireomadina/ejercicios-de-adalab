546bf53d49c2275f51ab0880146c389846b39b44
Leire Ordeñana <leireomadina@gmail.com>
2020-10-06 15:49:32 +0200

    Fix errors in dame ipsum exercise+

--- modulo-2/leccion-05-ejercicio-03-dame-ipsum/main.js
+++ modulo-2/leccion-05-ejercicio-03-dame-ipsum/main.js
@@ -7,9 +7,11 @@ const textEl2 = document.querySelector(".text-2");
 //handler
 function getNewParagraph () {
   const content = "<p>Aliquam posuere, nisi non porttitor tempor, orci velit vestibulum lorem, aliquet lobortis mi turpis non turpis. Suspendisse dictum dapibus nibh, vel iaculis quam tempus in. Curabitur sed aliquet lorem, a ullamcorper sem. Fusce justo lacus, sodales sed quam vitae, scelerisque laoreet quam. Quisque in dignissim enim. In et porta dui, quis tincidunt massa. Pellentesque eget vulputate augue.</p>";
-  content.in
-  return newContent;
+  textEl2.innerHTML = content;
+  console.log(content);
+  return content;
 }
 
 //listener on the html element
-text.addEventListener("mouseover", getNewParagraph);
+textEl.addEventListener("mouseover", getNewParagraph);
+console.log(content);
