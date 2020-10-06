6864faa588e29c19dd1f1bcfd14cbd46034bed11
Leire Ordeñana <leireomadina@gmail.com>
2020-10-06 10:14:48 +0200

    Improve box model calculator exercise

--- modulo-2/leccion-04-ejercicio-10-calculador-modelo-caja/main.js
+++ modulo-2/leccion-04-ejercicio-10-calculador-modelo-caja/main.js
@@ -1,24 +1,26 @@
 "use strict";
 
-function getBoxModel (bool, width, padding, border) {
+function getBoxModel(bool, width, padding, border) {
   //checks if it has the border-box model
   if (bool === true) {
     // border-box model to content-box
-    const contentModelWidth = width + (padding * 2) + (border * 2);
-    return `El ancho del contenido es ${width} y el ancho total de la caja es: ${contentModelWidth}`;
-  }  else { 
-    // content-box to border-box 
-  const borderModelWidth = width - (padding * 2) - (border * 2);
-  return `El ancho del contenido es ${width} y el ancho total de la caja es: ${borderModelWidth}`;
+    const contentModelWidth = width + padding * 2 + border * 2;
+    const result = `El ancho del contenido es ${width} y el ancho total de la caja es: ${contentModelWidth}`;
+    return result;
+  } else {
+    // content-box to border-box
+    const borderModelWidth = width - padding * 2 - border * 2;
+    const result2 = `El ancho del contenido es ${width} y el ancho total de la caja es: ${borderModelWidth}`;
+    return result2;
   }
 }
 
 // border-box model to content-box
-const boxExample = getBoxModel(true, 30, 1, 1 );
+const boxExample = getBoxModel(true, 30, 1, 1);
 console.log(boxExample);
 //returns "El ancho del contenido es 30 y el ancho total de la caja es: 34"
 
-// content-box to border-box 
-const boxExample2 = getBoxModel(false, 34, 1, 1 );
+// content-box to border-box
+const boxExample2 = getBoxModel(false, 34, 1, 1);
 console.log(boxExample2);
 //returns "El ancho del contenido es 34 y el ancho total de la caja es: 30"
