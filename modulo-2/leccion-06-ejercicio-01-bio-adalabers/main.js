64a3fcec45bd136e92e59435e98b4f57fa302d68
Leire Ordeñana <leireomadina@gmail.com>
2020-10-07 15:18:35 +0200

    Initial commit

---
+++ modulo-2/leccion-06-ejercicio-01-bio-adalabers/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const adalaber1 = {
+  name: "Susana",
+  age: 34,
+  job: "periodista",
+};
+
+const description = `Mi nombre es ${adalaber1.name}, tengo ${adalaber1.age} años y soy ${adalaber1.job}`;
+console.log(description);
+
+const adalaber2 = {
+  name: "Rocío",
+  age: 25,
+  job: "actriz",
+};
+
+const description2 = `${adalaber2.name}, ${adalaber2.age} años, ${adalaber2.job}`;
+console.log(description2);
