2c44d507b1b99fd4a76a66a6d8a75f947e3c239e
Leire Ordeñana <leireomadina@gmail.com>
2020-11-01 18:21:33 +0100

    Improve exercise 3 with render functions

--- modulo-3/leccion-01-ejercicio-03-calculo-geometrico-IV/main.js
+++ modulo-3/leccion-01-ejercicio-03-calculo-geometrico-IV/main.js
@@ -7,6 +7,16 @@ import Triangle from "./03-triangle.js";
 const mySquare = new Square(4);
 const myTriangle = new Triangle(4, 3);
 
-console.log(mySquare.perimeter());
-console.log(mySquare.area());
-console.log(myTriangle.area());
+//creamos una función para renderizar el perímetro de las instancias
+function renderPerimeter(object) {
+  console.log(object.perimeter());
+}
+
+//creamos otra función para renderizar el área
+function renderArea(object) {
+  console.log(object.area());
+}
+
+renderPerimeter(mySquare);
+renderArea(mySquare);
+renderArea(myTriangle);
