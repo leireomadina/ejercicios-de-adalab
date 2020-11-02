9df2e068614de670aef7fddfebbbca44fed67329
Leire Ordeñana <leireomadina@gmail.com>
2020-11-02 18:43:09 +0100

    Add exercise 2 except heart

--- modulo-3/leccion-03-ejercicio-02-mediacard-comp-II/mediacard/src/components/App.js
+++ modulo-3/leccion-03-ejercicio-02-mediacard-comp-II/mediacard/src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Mediacard from './Mediacard';
 import "../App.css";
+import sophie from '../sophie-hatter.jpg';
+
 
 function App() {
   return (
@@ -9,7 +11,22 @@ function App() {
         <h1 className="App-title">Mediacard</h1>
       </header>
       <main>
-        <Mediacard/>
+        <Mediacard
+        name="Sophie Hatter"
+        date="November 02, 2020"
+        image={sophie}
+        text1="Sophie Hatter is the female protagonist and heroine of the first book in the Castle Series, Howl's Moving Castle, and of the movie Howl's Moving Castle, directed by Hayao Miyazaki."
+        text2="Sophie is a young woman living in the small town of Market
+        Chipping in the kingdom of Ingary, and she is the eldest of three
+        sisters. In the land of Ingary, this, of course, means that Sophie
+        is destined to never find her fortune and live a dull life at
+        home. She works in her late father's hat shop as her stepmother,
+        Fanny Hatter's, apprentice."
+        likes="37"
+        /* 
+        Duda: ¿cómo hacer lo del corazón relleno o no usando font awesome?
+        */
+        />
       </main>
     </div>
   );
--- modulo-3/leccion-03-ejercicio-02-mediacard-comp-II/mediacard/src/components/Mediacard.js
+++ modulo-3/leccion-03-ejercicio-02-mediacard-comp-II/mediacard/src/components/Mediacard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import sophie from '../sophie-hatter.jpg';
 
 class Mediacard extends React.Component {
 
@@ -11,32 +10,25 @@ class Mediacard extends React.Component {
               className="card-image"
               alt="Imagen de Sophie Hatter"
               title="Imagen de Sophie Hatter"
-              src={sophie}
+              src={this.props.image}
             ></img>
             <div className="card-data">
-              <h2 className="card-name">Sophie Hatter</h2>
-              <p className="card-date">November 02, 2020</p>
+              <h2 className="card-name">{this.props.name}</h2>
+              <p className="card-date">{this.props.date}</p>
             </div>
           </header>
           <section className="card-body">
             <p className="card-text">
-              Sophie Hatter is the female protagonist and heroine of the first
-              book in the Castle Series, Howl's Moving Castle, and of the movie
-              Howl's Moving Castle, directed by Hayao Miyazaki.
+              {this.props.text1}
             </p>
             <p className="card-text">
-              Sophie is a young woman living in the small town of Market
-              Chipping in the kingdom of Ingary, and she is the eldest of three
-              sisters. In the land of Ingary, this, of course, means that Sophie
-              is destined to never find her fortune and live a dull life at
-              home. She works in her late father's hat shop as her stepmother,
-              Fanny Hatter's, apprentice.
+            {this.props.text2}
             </p>
           </section>
           <footer className="card-footer">
             <p className="card-footer-text">Read more...</p>
             <div className="card-likes">
-              <p className="card-number">37</p>
+              <p className="card-number">{this.props.likes}</p>
               <i class="fas fa-heart card-icon"></i>
             </div>
           </footer>
