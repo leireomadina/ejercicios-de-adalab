7bf802ab4406b42a298de4d875c16b963dc749e3
Leire Ordeñana <leireomadina@gmail.com>
2020-11-02 18:26:45 +0100

    Add exercise 2 (uncompleted)

---
+++ modulo-3/leccion-03-ejercicio-01-mediacard-comp-II/mediacard/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Mediacard from './Mediacard';
+import "../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Mediacard</h1>
+      </header>
+      <main>
+        <Mediacard/>
+      </main>
+    </div>
+  );
+}
+
+export default App;
