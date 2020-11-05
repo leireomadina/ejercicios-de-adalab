15a1f46c87bfe90f196487c93b47021c67d2483d
Leire Ordeñana <leireomadina@gmail.com>
2020-11-05 19:34:41 +0100

    Add handler function inside of component and bind it

--- modulo-3/leccion-05-ejercicio-03-onion/onion-hater/src/components/OnionHater.js
+++ modulo-3/leccion-05-ejercicio-03-onion/onion-hater/src/components/OnionHater.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-// EJERCICIO 1
-
+// EJERCICIO 1: teníamos la función handler fuera de la class OnionHater
+/*
 const handlerAlert = (event) => {
   // para acceder al valor del textarea usaremos event.target.value
   const usersText = event.target.value;
@@ -10,15 +10,35 @@ const handlerAlert = (event) => {
     alert("ODIO LA CEBOLLA");
   }
 };
+*/
 
 class OnionHater extends React.Component {
+  // incluimos el constructor para  asegurarnos de que el this que usará con la función handler es el this que queremos:
+  constructor(props) {
+    super(props);
+    // para ello usamos el método bind: este handler irá asociado siempre a nuestro componente OnionHater
+    this.handleAlert = this.handleAlert.bind(this);
+  }
+
+  handleAlert(event) {
+    // incluimos el método forceUpdate para que cada vez que se active el evento se vuelva a ejecutar render
+    this.forceUpdate();
+    // para acceder al valor del textarea usaremos event.target.value
+    const usersText = event.target.value;
+    // si el texto escrito por el usuario contiene la palabra "cebolla" saldrá nuestra alerta
+    if (usersText.includes("cebolla")) {
+      alert("ODIO LA CEBOLLA");
+    }
+  };
+
   render() {
     return (
-      <form className="form">
+      <form className="container">
         {/* queremos escuchar un evento desde el textarea: usaremos el evento change */}
         <textarea
           placeholder="Holi, cuéntame algo sobre las cebollas :)"
-          onChange={handlerAlert}
+          // incluimos el this ahora que el handler está dentro de nuestra clase OnionHater
+          onChange={this.handleAlert}
           className="textarea"
           name="textarea"
           id="textarea"
