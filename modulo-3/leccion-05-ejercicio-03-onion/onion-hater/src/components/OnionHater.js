fb0f148e578655714b8396487c9d52957cca7cf0
Leire Ordeñana <leireomadina@gmail.com>
2020-11-05 19:54:00 +0100

    Add conditional to change background color

--- modulo-3/leccion-05-ejercicio-03-onion/onion-hater/src/components/OnionHater.js
+++ modulo-3/leccion-05-ejercicio-03-onion/onion-hater/src/components/OnionHater.js
@@ -12,32 +12,40 @@ const handlerAlert = (event) => {
 };
 */
 
+//EJERCICIO 3: metemos la función handler dentro del componente de clase
+
 class OnionHater extends React.Component {
-  // incluimos el constructor para  asegurarnos de que el this que usará con la función handler es el this que queremos:
+  // incluimos el constructor para  asegurarnos de que el this que usará con la función handler es el this que queremos: para ello usamos el método bind, con lo que este handler irá asociado siempre a nuestro componente OnionHater
   constructor(props) {
     super(props);
-    // para ello usamos el método bind: este handler irá asociado siempre a nuestro componente OnionHater
     this.handleAlert = this.handleAlert.bind(this);
+    // Guardaremos la información de si estamos odiando o no
+    this.isHating = false;
   }
 
   handleAlert(event) {
     // incluimos el método forceUpdate para que cada vez que se active el evento se vuelva a ejecutar render
     this.forceUpdate();
+    // seleccionamos el contenedor que ocupa toda la página y cuyo fondo cambiará a color rojo
+    const container = document.querySelector(".container");
     // para acceder al valor del textarea usaremos event.target.value
     const usersText = event.target.value;
-    // si el texto escrito por el usuario contiene la palabra "cebolla" saldrá nuestra alerta
+    // si el texto escrito por el usuario contiene la palabra "cebolla" el fondo de la página se volverá rojo, y cuando se borre volverá al fondo blanco por defecto
     if (usersText.includes("cebolla")) {
-      alert("ODIO LA CEBOLLA");
+      container.classList.add("container-alert");
+      this.ishating = true;
+      // alert("ODIO LA CEBOLLA");
+    } else {
+      this.ishating = false;
+      container.classList.remove("container-alert");
     }
   };
 
   render() {
     return (
       <form className="container">
-        {/* queremos escuchar un evento desde el textarea: usaremos el evento change */}
         <textarea
           placeholder="Holi, cuéntame algo sobre las cebollas :)"
-          // incluimos el this ahora que el handler está dentro de nuestra clase OnionHater
           onChange={this.handleAlert}
           className="textarea"
           name="textarea"
