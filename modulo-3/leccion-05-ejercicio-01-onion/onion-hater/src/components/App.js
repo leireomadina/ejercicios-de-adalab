efdc8de305f09c7f9548670bd6da75227a7f8f30
Leire Ordeñana <leireomadina@gmail.com>
2020-11-05 18:13:55 +0100

    Add exercise 1 (uncompleted)

---
+++ modulo-3/leccion-05-ejercicio-01-onion/onion-hater/src/components/App.js
@@ -0,0 +1,12 @@
+import '../stylesheets/App.css';
+import OnionHater from './OnionHater';
+
+function App() {
+  return (
+    <div className="App">
+      <OnionHater></OnionHater>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo-3/leccion-05-ejercicio-01-onion/onion-hater/src/components/OnionHater.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class OnionHater extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <textarea placeholder="Holi, cuéntame algo :)" rows="20" cols="40"></textarea>
+      </div>
+    );
+  }
+}
+
+export default OnionHater;
