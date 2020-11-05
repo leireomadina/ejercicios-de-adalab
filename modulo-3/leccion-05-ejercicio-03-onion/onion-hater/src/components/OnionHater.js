bfbffdfad4024f70ef6a390b8a31b9d2002752dd
Leire Ordeñana <leireomadina@gmail.com>
2020-11-05 19:22:26 +0100

    initial commit

---
+++ modulo-3/leccion-05-ejercicio-03-onion/onion-hater/src/components/OnionHater.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+// EJERCICIO 1
+
+const handlerAlert = (event) => {
+  // para acceder al valor del textarea usaremos event.target.value
+  const usersText = event.target.value;
+  // si el texto escrito por el usuario contiene la palabra "cebolla" saldrá nuestra alerta
+  if (usersText.includes("cebolla")) {
+    alert("ODIO LA CEBOLLA");
+  }
+};
+
+class OnionHater extends React.Component {
+  render() {
+    return (
+      <form className="form">
+        {/* queremos escuchar un evento desde el textarea: usaremos el evento change */}
+        <textarea
+          placeholder="Holi, cuéntame algo sobre las cebollas :)"
+          onChange={handlerAlert}
+          className="textarea"
+          name="textarea"
+          id="textarea"
+        ></textarea>
+      </form>
+    );
+  }
+}
+
+export default OnionHater;
