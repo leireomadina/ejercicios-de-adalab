ef091d8f9d6fe9d314493148c86b25e7517be484
Leire Ordeñana <leireomadina@gmail.com>
2020-11-09 19:39:06 +0100

    Add alternative arrow function and finish exercise

--- modulo-3/leccion-06-ejercicio-02-cuadrado-parpadeante/src/components/App.js
+++ modulo-3/leccion-06-ejercicio-02-cuadrado-parpadeante/src/components/App.js
@@ -1,5 +1,5 @@
-import '../stylesheets/App.scss';
-import React from 'react';
+import "../stylesheets/App.scss";
+import React from "react";
 
 class App extends React.Component {
   constructor(props) {
@@ -7,12 +7,12 @@ class App extends React.Component {
     this.handleClick = this.handleClick.bind(this);
     // definimos el estado inicial: cuadrado de color azul construido con un objeto literal
     this.state = {
-      color: "blue"
-    }
+      color: "blue",
+    };
   }
 
   handleClick() {
-    // le pasamos a setState un callback o función  con dos parámetros:  prevState (el estado que vamos a modificar) y props (las del componente) 
+    // le pasamos a setState un callback o función  con dos parámetros:  prevState (el estado que vamos a modificar) y props (las del componente)
     this.setState((prevState, props) => {
       // esta variable definirá cual será el siguiente color o estado del cuadrado
       let nextColor;
@@ -22,17 +22,28 @@ class App extends React.Component {
         nextColor = "blue";
       }
       // devolvemos un objeto literal con la clave/nombre del estado (color) a cambiar: nos aseguramos de que el cuadrado podrá alternar entre los dos colores sin errores
-      return {color: nextColor}
-    })
+      return { color: nextColor };
+    });
   }
+
+  /*
+  // lo mismo que lo anterior pero con función ARROW 
+  handleClick() {
+    this.setState((prevState, props) => ({
+      color: prevState.color === "blue" ? "red" : "blue",
+    }));
+  }
+  */
 
   render() {
     const backgroundClass = this.state.color;
     return (
       <div className="App">
         <div className="container">
-          <div className={`square square-${backgroundClass}`} onClick={this.handleClick}>
-          </div>
+          <div
+            className={`square square-${backgroundClass}`}
+            onClick={this.handleClick}
+          ></div>
         </div>
       </div>
     );
