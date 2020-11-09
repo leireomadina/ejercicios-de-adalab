1fad29d0bed71b87078d2e9f290d2d58f27d8cfc
Leire Ordeñana <leireomadina@gmail.com>
2020-11-09 18:50:24 +0100

    Add event and status: exercise finished

--- modulo-3/leccion-06-ejercicio-01-input/src/components/App.js
+++ modulo-3/leccion-06-ejercicio-01-input/src/components/App.js
@@ -1,10 +1,38 @@
+import React from 'react';
 import '../stylesheets/App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // asociamos el evento a este componente de clase
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+    // definimos el estado inicial del componente usando un objeto literal
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleKeyUp(event) {
+    // recogemos lo que el usuario escribe en el input
+    const inputText = event.target.value;
+    this.inputText = inputText;
+    console.log(inputText);
+    // sustituimos el this.forceUpdate de los anteriores ejercicios por this.setState(): definimos qué sucede cuando el estado del input cambia, es decir, cuando su value cambia/cuando el usuario escriba algo
+    this.setState({
+      text: this.inputText
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <label htmlFor="text">¿En qué estás pensando?</label>
+        {/* usamos un evento de tipo keyUp */}
+        <input type="text" id="text" name="text" onKeyUp={this.handleKeyUp} className="input"></input>
+        <p>{this.state.text}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
