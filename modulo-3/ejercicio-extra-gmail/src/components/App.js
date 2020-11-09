0bb4fe3f7cddd50a938e28635115d2e3a569b604
Leire Ordeñana <leireomadina@gmail.com>
2020-11-09 10:41:16 +0100

    Add filters to renderEmails function

--- modulo-3/ejercicio-extra-gmail/src/components/App.js
+++ modulo-3/ejercicio-extra-gmail/src/components/App.js
@@ -4,58 +4,84 @@ import EmailItem from "./EmailItem";
 import EmailReader from "./EmailReader";
 import Header from "./Header";
 // Nuestro array con objetos está en la carpeta data. "emails" no va en mayúsculas porque no es un componente
-import emails from '../data/emails.json';
+import emails from "../data/emails.json";
 
-const renderEmails = () => {
-  //hacemos un map para recorrer los 3 elementos (objetos) del array, que está en la carpeta data y en un archivo llamado emails.json
-  return emails.map((email) => {
-    return (
-      <EmailItem
-        key={email.id}
-        from={email.fromName}
-        subject={email.subject}
-        time={email.date}
-      />
-    );
-  });
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // almacenaremos aquí la info que el usuario escribe en el input
+      inboxFilter: "",
+    };
+    this.handleInboxFilter = this.handleInboxFilter.bind(this);
+    this.handleDeleteFilter = this.handleDeleteFilter.bind(this);
+    this.handleTextFilter = this.handleTextFilter.bind(this);
+  }
 
-function App() {
-
-  const handleInboxFilter = () => {
+  // Es buena idea consolear aquí el state
+  handleInboxFilter() {
     console.log("App: me han clickado en recibidos");
   }
 
-  const handleDeleteFilter = () => {
+  handleDeleteFilter() {
     console.log("App: me han clickado en papelera");
   }
 
-  const handleTextFilter = (data) => {
-    console.log("App: me han clickado en campo de texto", data);
+  handleTextFilter(data) {
+    console.log("App: me han clickado en campo de texto", data.value);
+    // setState renderiza la página cada vez que el state es modificado
+    this.setState({
+      // la información que vamos a guardar aquí no depende del estado anterior, sino que vamos a machacar con el nuevo texto lo que haya en el this.state dentro del constructor
+      inboxFilter: data.value,
+    });
   }
 
-  return (
-    <div className="App">
-      <Header 
-        handleInboxFilter={handleInboxFilter}
-        handleDeleteFilter={handleDeleteFilter}
-        handleTextFilter={handleTextFilter}
-      />
+  renderEmails() {
+    const inboxFilter = this.state.inboxFilter.toLowerCase();
+    return emails
+      .filter((email) => {
+        return (
+          email.fromName.toLowerCase().includes(inboxFilter) ||
+          email.subject.toLowerCase().includes(inboxFilter) ||
+          email.body.toLowerCase().includes(inboxFilter)
+        );
+      })
+      .map((email) => {
+        return (
+          <EmailItem
+            key={email.id}
+            from={email.fromName}
+            subject={email.subject}
+            time={email.date}
+          />
+        );
+      });
+  }
 
-      <table className="table">
-        <tbody>
-          {renderEmails()}
-        </tbody>
-      </table>
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <Header
+          handleInboxFilter={this.handleInboxFilter}
+          handleDeleteFilter={this.handleDeleteFilter}
+          handleTextFilter={this.handleTextFilter}
+        />
+        <p>La usuario está filtrando por: {this.state.inboxFilter}</p>
 
-      <EmailReader 
-        subject={emails[0].subject}
-        fromName={emails[0].fromName}
-        fromEmail={emails[0].fromEmail}
-        body={emails[0].body}
-      />
-    </div>
-  );
+        <table className="table">
+          <tbody>{this.renderEmails()}</tbody>
+        </table>
+
+        <EmailReader
+          subject={emails[0].subject}
+          fromName={emails[0].fromName}
+          fromEmail={emails[0].fromEmail}
+          body={emails[0].body}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
