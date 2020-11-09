3dd100fb1b6d7ca488b59224d3b5172242164040
Leire Ordeñana <leireomadina@gmail.com>
2020-11-09 19:35:23 +0100

    Add setState with callback

--- modulo-3/leccion-06-ejercicio-02-cuadrado-parpadeante/src/components/App.js
+++ modulo-3/leccion-06-ejercicio-02-cuadrado-parpadeante/src/components/App.js
@@ -5,22 +5,33 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    // definimos el estado inicial: cuadrado de color azul construido con un objeto literal
     this.state = {
       color: "blue"
     }
   }
 
-  handleClick(event) {
-    this.setState({
-      color: "red"
-    });
+  handleClick() {
+    // le pasamos a setState un callback o función  con dos parámetros:  prevState (el estado que vamos a modificar) y props (las del componente) 
+    this.setState((prevState, props) => {
+      // esta variable definirá cual será el siguiente color o estado del cuadrado
+      let nextColor;
+      if (prevState.color === "blue") {
+        nextColor = "red";
+      } else {
+        nextColor = "blue";
+      }
+      // devolvemos un objeto literal con la clave/nombre del estado (color) a cambiar: nos aseguramos de que el cuadrado podrá alternar entre los dos colores sin errores
+      return {color: nextColor}
+    })
   }
 
   render() {
+    const backgroundClass = this.state.color;
     return (
       <div className="App">
         <div className="container">
-          <div className={`square square-${this.state.color}`} onClick={this.handleClick}>
+          <div className={`square square-${backgroundClass}`} onClick={this.handleClick}>
           </div>
         </div>
       </div>
