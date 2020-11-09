7d0fae9b7cec097b807fcee6de71f47ec4b9e738
Leire Ordeñana <leireomadina@gmail.com>
2020-11-09 12:17:36 +0100

    Add ProfesList component

--- modulo-3/leccion-04-ejercicios-en-clase/adalabers/src/components/App.js
+++ modulo-3/leccion-04-ejercicios-en-clase/adalabers/src/components/App.js
@@ -3,25 +3,40 @@ import '../stylesheets/App.css';
 import Weather from './Weather';
 import Today from './Today';
 import Tweet from './Tweet';
+import ProfesList from './ProfesList';
 
 class App extends React.Component {
   render() {
+
     // Definimos un objeto que pasaremos al componente Tweet (similar a pasar props, pero ahora las pasamos como un objeto)
     const tweetInfo = {
       twitterAccount: "igarridoatwork",
       tweetMessage: "Vamos a por React!!!",
     };
+    const profesoresList = ["Miguel", "Dayana", "Iván"];
+    const titleTeachers = "Profesores";
+
+    let queTiempoHaceHoy;
+    if (Math.random() > 0.5) {
+      queTiempoHaceHoy = "nuboso";
+    }
+    else {
+      queTiempoHaceHoy = "soleado";
+    }
+
     return (
       <div className="App">
         <header className="header">
           <h1 className="header__title">Componentes: Lista de Adalabers</h1>
         </header>
         <aside className="topBanner">
-          <Weather text="nuboso" />
+          <Weather text={queTiempoHaceHoy} />
           {/* buena práctica poner los valores numéricos entre corchetes para que ya sean values de tipo number y no haga falta hacer parteInt en un futuro */}
           <Today day={5} month={1} year={2020} />
           {/* Creamos dentro de tweetData una constante para no meter directamente ahí el objeto con la info a pasar al componente Tweet */}
           <Tweet tweetData={tweetInfo}/>
+          {/* Pasamos como props un array: lo hacemos como una variable para tener mayor claridad y limpieza */}
+          <ProfesList list={profesoresList} title={titleTeachers}/>
         </aside>
       </div>
     );
---
+++ modulo-3/leccion-04-ejercicios-en-clase/adalabers/src/components/ProfesList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class ProfesList extends React.Component {
+  render() {
+    // Primero definimos un array vacía
+    let html = [];
+    // Hacemos un bucle for para recorrer la lista de profesores, a la cual accedemos por props
+    for (let i = 0; i < this.props.list.length; i++) {
+      // Hacemos push sobre cada elemento del array. Añadir el atributo key para que cada item tenga un identificador único.
+      html.push(
+      <li key={i} id={i}>{this.props.list[i]}</li>
+      )
+    }
+
+  // También podemos usar un MAP en lugar de un buble for clásico
+  /*
+    const html = this.props.list.map(
+      function(profeItem, i) {
+      return (
+        <li key={i}>{profeItem}</li>
+        );
+      });
+      return html;
+  */
+
+    return (
+      <div className="profes_list">
+        <h3>{this.props.title}</h3>
+        <ul>
+          {html}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ProfesList;
