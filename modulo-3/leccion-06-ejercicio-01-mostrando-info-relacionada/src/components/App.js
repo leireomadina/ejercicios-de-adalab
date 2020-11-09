a7a0d096fbf12f0316d1bda23b4a696c1d895d02
Leire Ordeñana <leireomadina@gmail.com>
2020-11-09 18:59:27 +0100

    Clean unnecessary code and improve comments

--- modulo-3/leccion-06-ejercicio-01-mostrando-info-relacionada/src/components/App.js
+++ modulo-3/leccion-06-ejercicio-01-mostrando-info-relacionada/src/components/App.js
@@ -1,35 +1,34 @@
 import React from 'react';
-import '../stylesheets/App.css';
+import '../stylesheets/App.scss';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     // asociamos el evento a este componente de clase
-    this.handleKeyUp = this.handleKeyUp.bind(this);
-    // definimos el estado inicial del componente usando un objeto literal
+    this.handleChange = this.handleChange.bind(this);
+    // definimos el estado inicial del componente usando un objeto literal (vacío)
     this.state = {
       text: ""
     };
   }
 
-  handleKeyUp(event) {
+  handleChange(event) {
     // recogemos lo que el usuario escribe en el input
     const inputText = event.target.value;
-    this.inputText = inputText;
-    console.log(inputText);
     // sustituimos el this.forceUpdate de los anteriores ejercicios por this.setState(): definimos qué sucede cuando el estado del input cambia, es decir, cuando su value cambia/cuando el usuario escriba algo
     this.setState({
-      text: this.inputText
+      text: inputText
     });
   }
 
   render() {
+    const usersText = this.state.text;
     return (
       <div className="App">
         <label htmlFor="text">¿En qué estás pensando?</label>
-        {/* usamos un evento de tipo keyUp */}
-        <input type="text" id="text" name="text" onKeyUp={this.handleKeyUp} className="input"></input>
-        <p>{this.state.text}</p>
+        {/* usamos un evento de tipo Change */}
+        <input type="text" id="text" name="text" onChange={this.handleChange} className="input"></input>
+        <p className="paragraph">{usersText}</p>
       </div>
     );
   }
