c3b6eb445c7b1d20dbf70b689dd9fe162c0d275c
Leire Ordeñana <leireomadina@gmail.com>
2020-11-10 20:38:19 +0100

    Change folder name

--- modulo-3/leccion-07-ejercicio-01-y-02-hooks/src/components/App.js
+++ modulo-3/leccion-07-ejercicio-01-y-02-hooks/src/components/App.js
@@ -4,15 +4,19 @@ import '../stylesheets/App.scss';
 const App = () => {
 
   const [age, setAge] = useState(20);
+  const [gift, setGift] = useState(1);
 
   const handleClick = () => {
     setAge(age + 1);
+    setGift(gift +1);
   }
 
   return (
     <div className="App">
       <p>Hoy tengo {age} años de edad</p>
       <button onClick={handleClick}>Hazme más viejo</button>
+      <p>Tengo {gift} regalo</p>
+      <button onClick={handleClick}>Dame regalos</button>
     </div>
   );
 }
