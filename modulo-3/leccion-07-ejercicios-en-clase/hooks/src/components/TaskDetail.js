ff88792016b85ecdfbd837c7a86332194f14f5ec
Leire Ordeñana <leireomadina@gmail.com>
2020-11-11 09:38:50 +0100

    Add event and hooks

--- modulo-3/leccion-07-ejercicios-en-clase/hooks/src/components/TaskDetail.js
+++ modulo-3/leccion-07-ejercicios-en-clase/hooks/src/components/TaskDetail.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function TaskDetail(props) {
   console.log(props);
+  //state para saber si la tarea está completada o pendiente
+  const [styleTask, setStyleTask] = useState("pending");
   // Para comprobar si la tarea está pasada de fecha o no
   const styleDate = props.date <= 11 ? "hurry" : "";
-  const clickTask = (ev) => {};
+  const clickTask = (ev) => {
+    console.log("Me han hecho click" + props.subject);
+    //incluimos los estilos cuando una tarea está terminada al hacer click sobre ella
+    if (styleTask === "done") {
+      setStyleTask("pending");
+    } else {
+      setStyleTask("done");
+    }
+    //lo mismo pero con un operador ternario
+    // setStyleTask(styleTask === "done" ? "pending" : "done");
+  };
 
   return (
-    <div classname="list__item--details done">
+    <div classname={"list__item--details " + styleTask}>
       <input
         classname="list__item--check"
         type="checkbox"
