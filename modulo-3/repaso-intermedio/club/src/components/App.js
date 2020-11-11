4a75731272fcea3a1bb40eccab4cba6baad852ca
Leire Ordeñana <leireomadina@gmail.com>
2020-11-11 11:50:02 +0100

    Add template

---
+++ modulo-3/repaso-intermedio/club/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import React, { useState } from "react";
+import "../stylesheets/App.scss";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div classnameName="App"></div>;
+  }
+}
+
+export default App;
