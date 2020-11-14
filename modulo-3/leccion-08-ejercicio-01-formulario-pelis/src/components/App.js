fea3da9faa2118f445318c8afdb2caef45231aa8
Leire Ordeñana <leireomadina@gmail.com>
2020-11-14 19:39:42 +0100

    Finish form elements

--- modulo-3/leccion-08-ejercicio-01-formulario-pelis/src/components/App.js
+++ modulo-3/leccion-08-ejercicio-01-formulario-pelis/src/components/App.js
@@ -16,10 +16,12 @@ class App extends React.Component {
   }
 
   handleChange(event) {
-    const eventId = event.target.name;
-    const eventValue = event.target.value;
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    //asociamos dentro del estado cada name del form con cada value
     this.setState({
-      [eventId]: eventValue,
+      [name]: value,
     });
   }
 
@@ -77,22 +79,22 @@ class App extends React.Component {
               <div>
                 <p>Clasificación por edades:</p>
                 <label htmlFor="age">
-                  <input type="radio" id="age" value="all" name="age" checked />
+                  <input type="radio" id="age" value="Todos los públicos" name="age" onChange={this.handleChange}/>
                   Todos los públicos
                 </label>
                 <label htmlFor="age">
-                  <input type="radio" id="age" value="7" name="age" />7
+                  <input type="radio" id="age" value="7" name="age" onChange={this.handleChange}/>7
                 </label>
                 <label htmlFor="age">
-                  <input type="radio" id="age" value="12" name="age" />
+                  <input type="radio" id="age" value="12" name="age" onChange={this.handleChange}/>
                   12
                 </label>
                 <label htmlFor="age">
-                  <input type="radio" id="age" value="16" name="age" />
+                  <input type="radio" id="age" value="16" name="age" onChange={this.handleChange}/>
                   16
                 </label>
                 <label htmlFor="age">
-                  <input type="radio" id="age" value="18" name="age" />
+                  <input type="radio" id="age" value="18" name="age" onChange={this.handleChange}/>
                   18
                 </label>
               </div>
@@ -101,27 +103,27 @@ class App extends React.Component {
               <div>
                 <p>Género:</p>
                 <label htmlFor="genre">
-                  <input type="checkbox" id="genre" name="genre" value="comedy" />
+                  <input type="checkbox" id="genre" name="genre" value="Comedia" onChange={this.handleChange} />
                   Comedia
                 </label>
                 <label htmlFor="genre">
-                  <input type="checkbox" id="genre" name="genre" value="drama" />
+                  <input type="checkbox" id="genre" name="genre" value="Drama" onChange={this.handleChange}/>
                   Drama
                 </label>
                 <label htmlFor="genre">
-                  <input type="checkbox" id="genre" name="genre" value="fantasy" />
+                  <input type="checkbox" id="genre" name="genre" value="Fantasía" onChange={this.handleChange}/>
                   Fantasía
                 </label>
                 <label htmlFor="genre">
-                  <input type="checkbox" id="genre" name="genre" value="action" />
+                  <input type="checkbox" id="genre" name="genre" value="Acción" onChange={this.handleChange} />
                   Acción
                 </label>
                 <label htmlFor="genre">
-                  <input type="checkbox" id="genre" name="genre" value="family" />
+                  <input type="checkbox" id="genre" name="genre" value="Familia" onChange={this.handleChange} />
                   Familia
                 </label>
                 <label htmlFor="genre">
-                  <input type="checkbox" id="genre" name="genre" value="terror" />
+                  <input type="checkbox" id="genre" name="genre" value="Terror" onChange={this.handleChange} />
                   Terror
                 </label>
               </div>
@@ -132,8 +134,8 @@ class App extends React.Component {
               <h2>{this.state.title}</h2>
               <p>{this.state.description}</p>
               <p>{this.state.language}</p>
-              <p>Clasificación por edades</p>
-              <p>Género</p>
+              <p>{this.state.age}</p>
+              <p>{this.state.genre}</p>
             </article>
           </section>
         </main>
