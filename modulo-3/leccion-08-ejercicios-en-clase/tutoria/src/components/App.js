8b7b7c165366285cf1ba4d427edd14f80d392d77
Leire Ordeñana <leireomadina@gmail.com>
2020-11-15 13:19:44 +0100

    Add event in input component

--- modulo-3/leccion-08-ejercicios-en-clase/tutoria/src/components/App.js
+++ modulo-3/leccion-08-ejercicios-en-clase/tutoria/src/components/App.js
@@ -1,11 +1,22 @@
+import React, {useState} from "react";
 import '../stylesheets/App.scss';
 import Form from "./Form";
 
 const App = () => {
+  // Declaramos el estado inicial de las props mediante el hook useState
+  const [adress, setAdress] = useState("Henao");
+  // const adressStateInArray = useState("Henao"); // ["Henao", function ( {...})]
+  //const adress = adressStateInArray[0]
+  // const setAdress = adressStateInArray[1]
+  const [city, setCity] = useState("Bilbao");
+  console.log(adress);
+ 
   return (
     <div className="App">
       <h1>Props, lifting y estado</h1>
       <Form />
+      <p>Dirección: {adress}</p>
+      <p>Ciudad: {city}</p>
     </div>
   );
 }
--- modulo-3/leccion-08-ejercicios-en-clase/tutoria/src/components/Input.js
+++ modulo-3/leccion-08-ejercicios-en-clase/tutoria/src/components/Input.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 const Input = props => {
-  console.log(props.label);
+
+  const handleInputChange = (event) => {
+    console.log("Me han clickado", event.target.value);
+  }
+
+  // console.log(props.label);
   return (
     <div>
       <label htmlFor={props.id}>{props.label}</label>
-      <input type="text" name={props.name} id={props.id}></input>
+      <input type="text" name={props.name} id={props.id} onChange={handleInputChange}></input>
     </div>
   );
 };
