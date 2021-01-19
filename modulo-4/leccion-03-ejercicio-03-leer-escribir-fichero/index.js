743534b1fde9b13de50017e6dc39d94d3c18187a
Leire Ordeñana <leireomadina@gmail.com>
2021-01-19 13:07:52 +0100

    Add modify and write document logic

--- modulo-4/leccion-03-ejercicio-03-leer-escribir-fichero/index.js
+++ modulo-4/leccion-03-ejercicio-03-leer-escribir-fichero/index.js
@@ -1,11 +1,37 @@
-const fs = require('fs'); 
+const fs = require("fs");
 
-const handleFile = (error, fileContent) => {
-  if (error) {
-    console.log("Error:", error);
-  } else {
-    console.log("El contenido del fichero es:", fileContent);
-  }
-}
+const readFile = (fileName, callback) => {
+  fs.readFile(fileName, "utf-8", (error, data) => {
+    // console.log(fileName); // "./input-file.txt"
+    if (error) {
+      console.log("Error", error);
+    } else {
+      callback(data);
+    }
+  });
+};
 
-fs.readFile("input-file.txt", "utf-8", handleFile);
+const writeFile = (fileName, fileContent, callback) => {
+  fs.writeFile(fileName, fileContent, error => {
+    // console.log(fileName); // "./output-file.json"
+    if (error) {
+      console.log("Error", error);
+    } else {
+      callback();
+    }
+  });
+};
+
+readFile("./input-file.txt", fileContent => {
+  const objectContent = {
+    originalContent: fileContent,
+    changedContent: fileContent.toUpperCase(),
+    textLength: fileContent.length
+  };
+  
+  const newFileContent = JSON.stringify(objectContent);
+
+   writeFile("./output-file.json", newFileContent, () => {
+     console.log("The file has been copied :)");
+   })
+});
