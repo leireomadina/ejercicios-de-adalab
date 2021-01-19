ca3d2eb2fa5221190869c06431be311c97fc84ce
Leire Ordeñana <leireomadina@gmail.com>
2021-01-19 16:30:44 +0100

    Add node modules exercise 1

---
+++ modulo-4/leccion-04-ejercicio-02-libreria-math/index.js
@@ -0,0 +1,5 @@
+const { addNumbers, substractNumbers } = require("./math");
+const math = require("./math");
+
+console.log("Suma:", addNumbers(2,3));
+console.log("Resta:", substractNumbers(6, 2));
---
+++ modulo-4/leccion-04-ejercicio-02-libreria-math/math.js
@@ -0,0 +1,12 @@
+const addNumbers = (num1, num2) => {
+  return num1 + num2;
+};
+
+const substractNumbers = (num1, num2) => {
+  return num1 - num2;
+};
+
+module.exports = {
+  addNumbers: addNumbers,
+  substractNumbers: substractNumbers
+};
