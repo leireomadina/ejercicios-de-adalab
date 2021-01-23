a256b49c4cbbef7aff2ce110fb021bde73a2cf99
Leire Ordeñana <leireomadina@gmail.com>
2021-01-23 16:03:53 +0100

    Add email filter

---
+++ modulo-4/leccion-08-ejercicio-02-filtrar-nombre-email/public/js/main.js
@@ -0,0 +1,46 @@
+// "use strict";
+
+// post new user
+
+document
+  .querySelector(".js-btn-post-new-user")
+  .addEventListener("click", () => {
+    const inputName = document.querySelector(".js-input-name");
+    const inputEmail = document.querySelector(".js-input-email");
+
+    // create query params
+    const queryParams = `?userName=${inputName.value}&userEmail=${inputEmail.value}`;
+
+    fetch("http://localhost:3500/user" + queryParams, { method: "POST" })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log("Server response:", responseData);
+        printJson(".js-post-new-user-result", responseData);
+      });
+  });
+
+// get users data
+
+document.querySelector(".js-btn-get-users").addEventListener("click", () => {
+  const inputFilterName = document.querySelector(".js-input-name-filter");
+  const inputFilterEmail = document.querySelector(".js-input-email-filter");
+
+  // create query params
+  const queryParamsFilter = `?filterByName=${inputFilterName.value}&filterByEmail=${inputFilterEmail.value}`;
+  // console.log(queryParamsFilter);
+
+  fetch("http://localhost:3500/users" + queryParamsFilter, { method: "GET" })
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log("Server response:", responseData);
+      printJson(".js-get-users-result", responseData);
+    })
+    .catch((error) => console.log(`Ha sucedido un error: ${error}`));
+});
+
+// helper
+
+const printJson = (selector, jsonData) => {
+  const jsonHtml = JSON.stringify(jsonData, null, 2);
+  document.querySelector(selector).innerHTML = jsonHtml;
+};
---
+++ modulo-4/leccion-08-ejercicio-02-filtrar-nombre-email/src/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const cors = require("cors");
+
+// create server
+const server = express();
+
+// set express middleware
+server.use(express.json());
+server.use(cors());
+
+// init express aplication
+const serverPort = 3500;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// static server
+const staticServerPath = "./public";
+server.use(express.static(staticServerPath));
+
+// users
+
+const users = []; // fake users data base
+console.log(users);
+
+// api endpoints
+
+server.post("/user", (req, res) => {
+  console.log("Query params:", req.query);
+  console.log("Query param userName:", req.query.userName);
+  console.log("Query param userEmail:", req.query.userEmail);
+
+  // add new user to daba base
+  users.push({
+    name: req.query.userName,
+    email: req.query.userEmail,
+  });
+
+  res.json({
+    result: "User created",
+  });
+});
+
+server.get("/users", (req, res) => {
+  const queryParamFilterName = req.query.filterByName;
+  console.log('Query params filterByName:', queryParamFilterName);
+  const queryParamFilterEmail = req.query.filterByEmail;
+  console.log('Query params filterByEmail:', queryParamFilterEmail);
+  console.log(queryParamFilterName);
+  console.log(queryParamFilterEmail);
+
+  const filteredUsers = users.filter((user) =>
+    user.name.toUpperCase().includes(queryParamFilterName.toUpperCase()) && user.email.toUpperCase().includes(queryParamFilterEmail.toUpperCase())
+  );
+
+  res.json({
+    result: filteredUsers,
+  });
+});
