b199f1843c59e44ec598062f5c4b729b291b41fc
Leire Ordeñana <leireomadina@gmail.com>
2021-01-23 12:56:25 +0100

    Add new input and query params

--- modulo-4/leccion-08-ejercicio-01-filtrar-nombre/public/js/main.js
+++ modulo-4/leccion-08-ejercicio-01-filtrar-nombre/public/js/main.js
@@ -1,30 +1,37 @@
-'use strict';
+"use strict";
 
 // post new user
 
-document.querySelector('.js-btn-post-new-user').addEventListener('click', () => {
-  const inputName = document.querySelector('.js-input-name');
-  const inputEmail = document.querySelector('.js-input-email');
+document
+  .querySelector(".js-btn-post-new-user")
+  .addEventListener("click", () => {
+    const inputName = document.querySelector(".js-input-name");
+    const inputEmail = document.querySelector(".js-input-email");
 
-  // create query params
-  const queryParams = `?userName=${inputName.value}&userEmail=${inputEmail.value}`;
+    // create query params
+    const queryParams = `?userName=${inputName.value}&userEmail=${inputEmail.value}`;
 
-  fetch('http://localhost:3000/user' + queryParams, { method: 'POST' })
-    .then(response => response.json())
-    .then(responseData => {
-      console.log('Server response:', responseData);
-      printJson('.js-post-new-user-result', responseData);
-    });
-});
+    fetch("http://localhost:3000/user" + queryParams, { method: "POST" })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log("Server response:", responseData);
+        printJson(".js-post-new-user-result", responseData);
+      });
+  });
 
 // get users data
 
-document.querySelector('.js-btn-get-users').addEventListener('click', () => {
-  fetch('http://localhost:3000/users')
-    .then(response => response.json())
-    .then(responseData => {
-      console.log('Server response:', responseData);
-      printJson('.js-get-users-result', responseData);
+document.querySelector(".js-btn-get-users").addEventListener("click", () => {
+  const inputFilterName = document.querySelector(".js-input-name-filter");
+
+  // create query params
+  const queryParamsFilter = `?filterByName=${inputFilterName.value}`;
+
+  fetch("http://localhost:3000/users")
+    .then((response) => response.json())
+    .then((responseData) => {
+      console.log("Server response:", responseData);
+      printJson(".js-get-users-result", responseData);
     });
 });
 
